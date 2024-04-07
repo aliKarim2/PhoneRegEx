dec98949659f37e8aebba146cd7b7d40874ddec3
aliKarim2 <222alikarim@gmail.com>
2024-04-07 15:40:44 -0700

    add basic functions

--- regex.cpp
+++ regex.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
+#include <string>
 
-
+bool phoneCheck(std::string phoneNumber);
+bool areaCodeCheck(std::string areaCode);
+bool exchangeCodeCheck(std::string exchangeCode);
+bool additionalDigitCheck(std::string additionalDigits);
+bool extensionCheck(std::string extnesion);
 
 int main(){
 
-    std::cout << "Program Terminated";
+    std::string input;
 
-  return 0;
+    input = "reads";
+
+
+    std::cout << (phoneCheck(input) ? "Good" : "Bad");
+
+
+
+
+
+    std::cout << "\nProgram Terminated";
+    return 0;
+}
+
+bool phoneCheck(std::string phoneNumber){
+    std::cout << "Inputted Number: " << phoneNumber << "\n";
+    
+
+    
+
+
+
+    // exchangeCodeCheck();
+    // additionalDigitCheck();
+    // extensionCheck();
+    
+
+    return 1;
+}
+bool areaCodeCheck(std::string areaCode){
+
+    return 0;
+}
+bool exchangeCodeCheck(std::string exchangeCode){
+
+    return 0;
+}
+bool additionalDigitCheck(std::string additionalDigits){
+
+    return 0;
+}
+bool extensionCheck(std::string extnesion){
+
+    return 0;
 }
