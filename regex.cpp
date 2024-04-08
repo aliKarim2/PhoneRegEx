1b994730a4ff304688fde4343fa44247fb451b88
aliKarim2 <222alikarim@gmail.com>
2024-04-08 15:46:07 -0700

    add exchange code check

--- regex.cpp
+++ regex.cpp
@@ -40,7 +40,6 @@ bool phoneCheck(std::string phoneNumber){
     const bool PARENTHESIS = true;
     char seperator;
     std::cout << "Inputted Number: " << phoneNumber << "\n";
-    std::string focusedInput;
     // phoneNumber = phoneNumber.substr(5);
 
 
@@ -56,7 +55,7 @@ bool phoneCheck(std::string phoneNumber){
     //Check the area code
     {
         if(phoneNumber[0] == '('){
-            phoneNumber = phoneNumber.substr(5);
+            // phoneNumber = phoneNumber.substr(5);
 
             // std::cout << focusedInput << '\n';
             // std::cin.get();
@@ -69,7 +68,10 @@ bool phoneCheck(std::string phoneNumber){
             //if the area code is surrounded with parenthesis
             //it should be 5 characters: 2 parenthesis and 3 nums
             std::string input = phoneNumber.substr(0,5);
-             if(!areaCodeCheck(input, PARENTHESIS)){
+             if(!areaCodeCheck(phoneNumber, PARENTHESIS)){
+                #if TEST_COMMENTS
+                std::cout << "bad area code\n";
+                #endif
                 return 0;
              }
              phoneNumber = phoneNumber.substr(5);
@@ -85,10 +87,13 @@ bool phoneCheck(std::string phoneNumber){
             //we will check if the number is valid (2-9) in function
             //it should be 3 characters long, just numbers
             std::string input = phoneNumber.substr(0,3);
-            if(areaCodeCheck(input, !PARENTHESIS)){
+            if(areaCodeCheck(phoneNumber, !PARENTHESIS)){
+                #if TEST_COMMENTS
+                std::cout << "bad area code\n";
+                #endif
                 return 0;
             }
-            
+
             phoneNumber = phoneNumber.substr(3);
         }
         else{
@@ -103,23 +108,36 @@ bool phoneCheck(std::string phoneNumber){
 
 
     //check to see what the seperator symbol is
-    if(focusedInput[0] == '.' || focusedInput[0] == '-'){
-        seperator = focusedInput[0];
+    if(phoneNumber[0] == '.' || phoneNumber[0] == '-'){
+        seperator = phoneNumber[0];
     } else {
         return 0;
     }
 
+    //make the new phone number start from the exchange number
+    phoneNumber = phoneNumber.substr(1);
+
+
     //Check the exchange numbers
     {
-        exchangeCodeCheck(focusedInput);
+        exchangeCodeCheck(phoneNumber);
+
+        //remove the exchange code from string
+        phoneNumber = phoneNumber.substr(3);
+
+    }
+    //check for next seperator
+    if(phoneNumber[0] != seperator){
+        return 0;
     }
 
+    //make phone number just start from the additional digits
+    phoneNumber = phoneNumber.substr(1);
 
 
 
 
 
-    // exchangeCodeCheck();
     // additionalDigitCheck();
     // extensionCheck();
     
@@ -133,21 +151,26 @@ bool areaCodeCheck(std::string areaCode, bool paranthesis){
         //Notice we dont have to check for first parenthesis
         //bc that happened in the phone check fxn
 
-        //first number check`
+
+
+        //first number check
         if(!(areaCode[1] >= '2' && areaCode[1] <= '9')){
             //if first digit not from 0-9, return false
             return 0;
         }
 
 
+
         //last 2 number check
-        for(int i = 2; i < 4; i++){
+        std::cin.get();
+        for(int i = 2; i <= 3; i++){
             //check if last 2 numbers are digits
             if(!isdigit(areaCode[i])){  
                 return false;
             }
         }
 
+        std::cout << "C1\n";
 
         if(areaCode[4] != ')'){
             return false;
@@ -182,6 +205,15 @@ bool areaCodeCheck(std::string areaCode, bool paranthesis){
     return 0;
 }
 bool exchangeCodeCheck(std::string exchangeCode){
+
+    //checks for 3 numbers at start of a string
+    //if there are not 3 consecutive numbers, fails
+    for(int i = 0; i < 3; i++){
+        if(!isdigit(exchangeCode[i])){
+            return 0;
+        }
+    }
+
 
     return 0;
 }
